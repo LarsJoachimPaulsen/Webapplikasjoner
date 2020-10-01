7ffa0c8446dadee0fb266ffc271dbd1cb6231414
LarsJoachimPaulsen <paulsen.lars@gmail.com>
2020-10-01 11:40:44 +0200

    init

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Title from './src/components/Title'; 
+
+
+const greeting = 'Todo'; 
+
+const App = () => (
+    <>
+        <Title title = {greeting}/>
+    </>
+);
+
+export default App;
