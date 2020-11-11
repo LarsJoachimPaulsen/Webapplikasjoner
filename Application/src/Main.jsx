98afed5298944d3d5693e8a74c9fb3d4cdbeece0
LarsJoachimPaulsen <paulsen.lars@gmail.com>
2020-11-11 21:19:43 +0100

    react styling

--- Application/src/Main.jsx
+++ Application/src/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import Route from '../Routes/Routes';
+import Theme from '../CommonComponents/Styles/Theme';
 
 const Main = () =>(
-   <Route/>
+   <Theme>
+      <Route/>
+   </Theme>
         
 );
 
