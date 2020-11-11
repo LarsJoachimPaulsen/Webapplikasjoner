456cabccd9f7928d0e115da5118e654e68a56a1d
LarsJoachimPaulsen <paulsen.lars@gmail.com>
2020-11-11 21:33:27 +0100

    styles

--- Application/CommonComponents/Nav.jsx
+++ Application/CommonComponents/Nav.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
+const StyledNav = styled.nav`
+    width: 100%; 
+    
+`;
+
+const NavMenu = styled.ul`
+    display: flex; 
+    justify-content: space-between;
+    margin-right: 20px;  
+    padding: 0; 
+    list-style: none; 
+`;
+
+const NavMenuItem = styled.li`
+    padding: 0; 
+`;
 const Navbar = () =>(
-    <nav> 
-        <h1> Oblig 6</h1>   
-        <div> 
-            <p><NavLink to="/"> Startside</NavLink></p>
-            <p><NavLink to="/login">Lag bruker</NavLink></p>
-            <p><NavLink to="/poll"> Svar på poll</NavLink></p>
-            <p><NavLink to="/createPoll"> Lag ny poll</NavLink></p>
-        </div> 
-     </nav>
+
+    <StyledNav>    
+        <NavMenu>
+            <NavMenuItem>
+                <nav> 
+                    <h1> Oblig 6</h1>   
+                    <div> 
+                       <NavLink to="/"> Startside</NavLink>
+                       <NavLink to="/login">Lag bruker</NavLink>
+                        <NavLink to="/poll"> Svar på poll</NavLink>
+                        <NavLink to="/createPoll"> Lag ny poll</NavLink>
+                    </div> 
+                </nav>
+            </NavMenuItem>
+        </NavMenu>
+     </StyledNav>
+
 )
 
 export default Navbar;
