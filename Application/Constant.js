49da716f922b443da4c88088eed8d55599ee47f9
LarsJoachimPaulsen <paulsen.lars@gmail.com>
2020-11-12 01:05:16 +0100

    API calls, needs fix

---
+++ Application/Constant.js
@@ -0,0 +1,9 @@
+import 'dotenv/config.js'
+
+const PORT = process.env.PORT;
+
+const SERVERPORT = process.env.SERVERPORT; 
+
+const BASE_API_URL = process.env.BASE_API_URL; 
+
+export {PORT, SERVERPORT, BASE_API_URL}
