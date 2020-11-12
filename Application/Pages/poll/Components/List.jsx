a4032700a410a61300c091d4302dd680b8ed1a92
LarsJoachimPaulsen <paulsen.lars@gmail.com>
2020-11-12 23:54:25 +0100

    started styled List component

--- Application/Pages/poll/Components/List.jsx
+++ Application/Pages/poll/Components/List.jsx
@@ -1,4 +1,11 @@
 import React from 'react'; 
+import styled from 'styled-components'; 
+
+const ListStyle = styled.li`
+
+`; 
+
+
 
 const List = ({ data, answerbtn }) => (
 
@@ -7,14 +14,15 @@ const List = ({ data, answerbtn }) => (
             <li key={item[0]}>
                 <header>
                     <h2 name="pollname">{item[1].pollname}</h2>
-                    <p name="question">{item[1].question}</p>
-                    <ul>
-                        <li> {item[1].answers[0]}</li>
-                        <li> {item[1].answers[1]}</li>
-                        <li> {item[1].answers[2]}</li>
-                        <li> {item[1].answers[3]}</li>
-                    </ul>
                 </header>
+                <p name="question">{item[1].question}</p>
+                <ul>
+                    <li> {item[1].answers[0]}</li>
+                    <li> {item[1].answers[1]}</li>
+                    <li> {item[1].answers[2]}</li>
+                    <li> {item[1].answers[3]}</li>
+                </ul>
+                
 
                 <button onClick={answerbtn}> Answer </button>
             </li>
